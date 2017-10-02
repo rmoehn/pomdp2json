03efae69b807ecba6178cb4446048025082c8063
Richard Möhn <richard.moehn@posteo.de>
2017-10-03 07:17:55 +0900

    pomdp2json.py: Change license notice
    The GPL included with the original code is version 2 and doesn't say
    anything about future versions. I assume that the license notices in the
    source code have to match that. So remove the future versions part from
    the license notice in pomdp2json.py.

--- libPOMDP/pomdp2json.py
+++ libPOMDP/pomdp2json.py
@@ -6,8 +6,7 @@ Copyright (C) 2017  Richard Möhn <richard.moehn+p2j@posteo.de>
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
+as published by the Free Software Foundation, version 2.
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
